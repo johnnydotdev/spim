af5a78686c60fae6e81d2448639b61cab898da95
SudarshanGp <SudarshanGp@users.noreply.github.com>
2015-05-24 11:30:23 -0400

    Update lookup.s

--- sperm/LabSpimbot/lookup.s
+++ sperm/LabSpimbot/lookup.s
@@ -17,26 +17,3 @@
 ##     word ++;
 ##     return lookup_word_in_trie(next_trie, word);
 ## }
-
-.globl lookup_word_in_trie
-lookup_word_in_trie:
-	beq	$a0, 0, lwit_return_null	# trie == NULL
-	lw	$v0, 0($a0)			# trie->word
-	beq	$v0, 0, lwit_not_word		# !(trie->word)
-	jr	$ra
-
-lwit_not_word:
-	lbu	$t0, 0($a1)			# *word
-	sub	$t0, $t0, 'A'			# c = *word - 'A'
-	blt	$t0, 0, lwit_return_null	# c < 0
-	bge	$t0, 26, lwit_return_null	# c >= 26
-
-	mul	$t0, $t0, 4			# c * 4
-	add	$t0, $a0, $t0			# &trie->next[c] - 4
-	lw	$a0, 4($t0)			# next_trie = trie->next[c]
-	add	$a1, $a1, 1			# word ++
-	j	lookup_word_in_trie
-
-lwit_return_null:
-	li	$v0, 0
-	jr	$ra
